1c94e65debb2c3f0610f7aa3baffebc86b59e015
Eziz Rehimov <fantastig@bk.ru>
2021-06-04 14:29:57 +0400

    Second page created

---
+++ app/src/main/java/com/eziz/loginapp/MainRegisterActivity.kt
@@ -0,0 +1,11 @@
+package com.eziz.loginapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainRegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_register)
+    }
+}
